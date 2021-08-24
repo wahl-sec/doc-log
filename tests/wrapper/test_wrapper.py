46dd44c01901fee69c5aca4f0a0d8be7c273fee3
Jacob Wahlman <mr.jacobwahlman+git@gmail.com>
2021-08-24 17:57:39 +0200

    Added some wrapper tests

---
+++ tests/wrapper/test_wrapper.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from doc_log import doc_log
+
+
+def test_wrapper_pep257_style_simple_passive():
+    @doc_log(dialect="pep257", type_check=True)
+    def _test_func(i, j=0) -> int:
+        """Function that adds two numbers and returns the result.
+
+        Arguments:
+        i -- the first number
+
+        Keyword Arguments:
+        j -- the second number (default 0)
+
+        Types:
+        i -- int
+        j -- int
+
+        Returns:
+        Result of addition between `i` and `j`.
+
+        Return Type:
+        int
+        """
+        return i + j
+
+    assert _test_func(2) == 2
+
+
+def test_wrapper_epytext_style_simple_passive():
+    @doc_log(dialect="epytext", type_check=True)
+    def _test_func(i, j=0) -> int:
+        """
+        Function that adds two numbers and returns the result.
+
+        @param i: the first number
+        @type i: int
+        @param j: the second number
+        @type j: int
+        @return: Result of addition between `i` and `j`.
+        @rtype: int
+        """
+        return i + j
+
+    assert _test_func(2) == 2
+
+
+def test_wrapper_rest_style_simple_passive():
+    @doc_log(dialect="rest", type_check=True)
+    def _test_func(i, j=0) -> int:
+        """Function that adds two numbers and returns the result.
+
+        :param i: the first number
+        :type i: int
+        :param j: the second number
+        :type j: int
+        :return: Result of addition between `i` and `j`.
+        :rtype: int
+        """
+        return i + j
+
+    assert _test_func(2) == 2
+
+
+def test_wrapper_google_style_simple_passive():
+    @doc_log(dialect="google", type_check=True)
+    def _test_func(i, j=0) -> int:
+        """Function that adds two numbers and returns the result.
+
+        Args:
+            i: the first number
+            j: the second number
+
+        Types:
+            i: int
+            j: int
+
+        Returns:
+            Result of addition between `i` and `j`.
+
+        Return Type:
+            int
+        """
+        return i + j
+
+    assert _test_func(2) == 2
+
+
+def test_wrapper_numpydoc_style_simple_passive():
+    @doc_log(dialect="numpydoc", type_check=True)
+    def _test_func(i, j=0) -> int:
+        """Function that adds two numbers and returns the result.
+
+        Parameters
+        ----------
+        i : int
+            the first number
+        j : int
+            the second number
+
+        Returns
+        -------
+        int
+            Result of addition between `i` and `j`.
+        """
+        return i + j
+
+    with pytest.raises(NotImplementedError):
+        assert _test_func(2) == 2
